c305d615049e0177d337efe56019c15199082a73
Doug MacEwen <d2macewen@gmail.com>
2019-11-02 11:59:23 -0700

    breaking spectrum out a little

--- analyze_raw.py
+++ analyze_raw.py
@@ -4,16 +4,24 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 
-#imgName = 'sky5'
-#imgName = 'sunRef2' #Possibly Perfect
-imgName = 'sunRef3' #Possibly Perfect
-##imgName = 'sunRef4' #Under Exposed
-##imgName = 'sunRef5' #Under Exposed
-##imgName = 'sunRef6' #Over Exposed
+#skyImg = 'sky5'
+##imgName = 'sunRef2' #Possibly Perfect
+#sunImg = 'sunRef3' #Possibly Perfect
+###imgName = 'sunRef4' #Under Exposed
+###imgName = 'sunRef5' #Under Exposed
+###imgName = 'sunRef6' #Over Exposed
 ##imgName = 'sunRef7' #Over Exposed
-#imgName = 'iPad1'
-#imgName = 'iPad_ScreenFlash1'
-#imgName = 'benQ1'
+#iPadImg = 'iPad1'
+##imgName = 'iPad_ScreenFlash1'
+##imgName = 'benQ1'
+#stoveImg = 'StoveLight'
+#skyImg = 'sky'
+#skyImg = 'purpleDim'
+#skyImg = 'purpleBright'
+sunDimImg = 'sunlightDim'
+sunMediumImg = 'sunlightMedium'
+#sunDimImg = 'sunlightDim'
+benQImg = 'BenQ'
 
 
 def stretch(img, mask=None):
@@ -21,15 +29,19 @@ def stretch(img, mask=None):
     minVal = np.min(img[mask])
     maxVal = np.max(img[mask])
 
-    spread = maxVal - minVal
-    stretched = (img - minVal) / spread
+    magnitude = maxVal - minVal
+    stretched = (img - minVal) / magnitude
     stretched[np.logical_not(mask)] = 0
-    return stretched, spread
+    return stretched#, magnitude
 
-def autocrop(img, redMask, greenMask, blueMask):
-    scaled, _ = stretch(img)
+def autocrop(img, threshold, redMask, greenMask, blueMask):
+    scaled = stretch(img)
 
-    threshold = 0.95
+    #med = np.median(scaled[scaled > (10/255)])
+    #print('Med :: {}'.format(med))
+
+    #threshold = 0.30
+    #threshold = 0.50
     brightSubpixelMask = scaled > threshold
     brightSubpixelMask = brightSubpixelMask.astype('uint8') * 255
 
@@ -46,56 +58,72 @@ def autocrop(img, redMask, greenMask, blueMask):
 
     return [numbersBB, spectrumBB]
 
-with rawpy.imread('images/{}.DNG'.format(imgName)) as raw:
-    redMask = raw.raw_colors == 0
-    #greenMask = np.logical_or((raw.raw_colors == 1), (raw.raw_colors == 3))
-    greenMask = raw.raw_colors == 1
-    blueMask = raw.raw_colors == 2
+def extractSpectrum(spectrum, mask, stretch=True):
+    img = np.copy(spectrum)
+    if stretch:
+        img = stretch(img, mask)
+    else:
+        img[np.logical_not(mask)] = 0
+    rowMask = np.any(img.T, axis=0)
+    medians = np.median(img.T[:, rowMask], axis=1)
+    medians = medians[medians > 0]
+    return [img, medians]
 
-    numbersBB, spectrumBB = autocrop(raw.raw_image, redMask, greenMask, blueMask)
+def extractSpectrums(imgFileName, threshold):
+    with rawpy.imread('imagesRed/{}.DNG'.format(imgFileName)) as raw:
+        redMask = raw.raw_colors == 0
+        #greenMask = np.logical_or((raw.raw_colors == 1), (raw.raw_colors == 3))
+        greenMask = raw.raw_colors == 1
+        blueMask = raw.raw_colors == 2
 
-    numbers = np.copy(raw.raw_image[numbersBB[1]:(numbersBB[1] + numbersBB[3]), numbersBB[0]:(numbersBB[0] + numbersBB[2])])
-    spectrum = np.copy(raw.raw_image[spectrumBB[1]:(spectrumBB[1] + spectrumBB[3]), spectrumBB[0]:(spectrumBB[0] + spectrumBB[2])])
+        numbersBB, spectrumBB = autocrop(raw.raw_image, threshold, redMask, greenMask, blueMask)
+
+        numbers = np.copy(raw.raw_image[numbersBB[1]:(numbersBB[1] + numbersBB[3]), numbersBB[0]:(numbersBB[0] + numbersBB[2])])
+        spectrum = np.copy(raw.raw_image[spectrumBB[1]:(spectrumBB[1] + spectrumBB[3]), spectrumBB[0]:(spectrumBB[0] + spectrumBB[2])])
+
+    numbersRedMask = redMask[numbersBB[1]:(numbersBB[1] + numbersBB[3]), numbersBB[0]:(numbersBB[0] + numbersBB[2])]
 
     redMask = redMask[spectrumBB[1]:(spectrumBB[1] + spectrumBB[3]), spectrumBB[0]:(spectrumBB[0] + spectrumBB[2])]
     greenMask = greenMask[spectrumBB[1]:(spectrumBB[1] + spectrumBB[3]), spectrumBB[0]:(spectrumBB[0] + spectrumBB[2])]
     blueMask = blueMask[spectrumBB[1]:(spectrumBB[1] + spectrumBB[3]), spectrumBB[0]:(spectrumBB[0] + spectrumBB[2])]
 
-    numbers, _ = stretch(numbers)
+    numbers = stretch(numbers, numbersRedMask)
 
-    redImg = np.copy(spectrum)
-    redImg, redRange = stretch(redImg, redMask)
-    print('Red Range :: {}'.format(redRange))
-    redRowMask = np.any(redImg.T, axis=0)
-    redMedians = np.median(redImg.T[:, redRowMask], axis=1)
-    redMedians = redMedians[redMedians > 0]
 
-    greenImg = np.copy(spectrum)
-    greenImg, greenRange = stretch(greenImg, greenMask)
-    print('Green Range :: {}'.format(greenRange))
-    greenRowMask = np.any(greenImg.T, axis=0)
-    greenMedians = np.median(greenImg.T[:, greenRowMask], axis=1)
-    greenMedians = greenMedians[greenMedians > 0]
+    stretched = stretch(spectrum)
 
-    blueImg = np.copy(spectrum)
-    blueImg, blueRange = stretch(blueImg, blueMask)
-    print('Blue Range :: {}'.format(blueRange))
-    blueRowMask = np.any(blueImg.T, axis=0)
-    blueMedians = np.median(blueImg.T[:, blueRowMask], axis=1)
-    blueMedians = blueMedians[blueMedians > 0]
+    #redImg, redMagnitude, redMedians = extractSpectrum(spectrum, redMask, False)
+    redImg, redMedians = extractSpectrum(stretched, redMask, False)
+    #print('Red Magnitude :: {}'.format(redMagnitude))
 
+    #greenImg, greenMagnitude, greenMedians = extractSpectrum(spectrum, greenMask, False)
+    greenImg, greenMedians = extractSpectrum(stretched, greenMask, False)
+    #print('Green Magnitude :: {}'.format(greenMagnitude))
+
+    #blueImg, blueMagnitude, blueMedians = extractSpectrum(spectrum, blueMask, False)
+    blueImg, blueMedians = extractSpectrum(stretched, blueMask, False)
+    #print('Blue Magnitude :: {}'.format(blueMagnitude))
+
+    return [numbers, [redImg, redMedians], [greenImg, greenMedians], [blueImg, blueMedians]]
+
+def spectrumCheck(imgFileName, threshold):
+    numbers, red, green, blue = extractSpectrums(imgFileName, threshold)
 
     fig = plt.figure()
     ax1 = fig.add_axes([0.1, 0.5, 0.85, 0.5])
-    ax1.plot(np.arange(len(redMedians)), redMedians, 'r-')
-    ax1.plot(np.arange(len(greenMedians)), greenMedians, 'g-')
-    ax1.plot(np.arange(len(blueMedians)), blueMedians, 'b-')
+    ax1.plot(np.arange(len(red[1])), red[1], 'r-')
+    ax1.plot(np.arange(len(green[1])), green[1], 'g-')
+    ax1.plot(np.arange(len(blue[1])), blue[1], 'b-')
 
     ax2 = fig.add_axes([0.1, 0, 0.85, 0.5])
-    ax2.imshow(np.vstack([numbers, redImg, greenImg, blueImg]), cmap='gray')#, interpolation='nearest')
+    ax2.imshow(np.vstack([numbers, red[0], green[0], blue[0]]), cmap='gray')
+    #ax2.imshow(numbers, cmap='gray')
 
     fig.show()
 
-    imgs = np.vstack([numbers, redImg, greenImg, blueImg])
-    cv2.imshow('RGB', imgs)
-    cv2.waitKey(0)
+#spectrumCheck(sunDimImg, 0.30)
+spectrumCheck(sunMediumImg, 0.50)
+spectrumCheck(benQImg, 0.30)
+#spectrumCheck(iPadImg)
+cv2.imshow('Force Pause', np.ones([100, 100, 1], dtype='uint8'))
+cv2.waitKey(0)
