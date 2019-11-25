dfea192cd17477c544563a772bc0b4b662edda80
Doug MacEwen <d2macewen@gmail.com>
2019-11-25 15:23:56 -0800

    auto cropping properly

--- imageToSpectrum.py
+++ imageToSpectrum.py
@@ -5,17 +5,13 @@ import matplotlib.pyplot as plt
 from scipy.signal import savgol_filter
 import math
 
-skyImg = 'sky'
-sunDimImg = 'sunlightDim' 
-benQImg = 'BenQ2'
-iPadImg = 'iPad' #400nm -> 710nm
-#ncadecentAImg = 'IncadecentA'
-#ncadecentBImg = 'IncadecentB'
-#incadecentBBrightImg = 'IncadecentB_bright'
-#ledImg = 'LED' 
-incadecentAImg = 'IncadecentA_card'
-incadecentBImg = 'IncadecentB_card'
-ledImg = 'LED_card' 
+skyImg = ['sky', 0.5]
+sunDimImg = ['sunlightDim', 0.08]
+benQImg = ['BenQ2', 0.1]
+iPadImg = ['iPad', 0.3] #400nm -> 710nm
+incadecentAImg = ['IncadecentA_card', 0.5]
+incadecentBImg = ['IncadecentB_card', 0.5]
+ledImg = ['LED_card' , 0.5]
 
 def stretch(img, mask=None):
     mask = mask if mask is not None else np.ones(img.shape, dtype='bool')
@@ -42,7 +38,7 @@ def autoBB(img, threshold, redMask, greenMask, blueMask):
 
     numbersBB = cv2.boundingRect(contours[largestContour])
     #spectrumBB = numbersBB + np.array([0, int(1.5 * numbersBB[3]), 0, 0)]) #Just offset the numberline by 2 times its height. Samples roughly the middle of the spectrum area
-    spectrumBB = numbersBB + np.array([0, int(1.75 * numbersBB[3]), 0, int(-0.5 * numbersBB[3])]) #Just offset the numberline by 2 times its height. Samples roughly the middle of the spectrum area
+    spectrumBB = numbersBB + np.array([0, int(1 * numbersBB[3]), 0, int(-0.5 * numbersBB[3])]) #Just offset the numberline by 2 times its height. Samples roughly the middle of the spectrum area
 
     return [numbersBB, spectrumBB]
 
@@ -74,8 +70,8 @@ def loadRawImage(filename):
 
 
 #Crop is [[Y, X], [Height, Width], HeightRatio]
-def extractSpectrums(imgFileName, threshold, crop=None):
-    with rawpy.imread('imagesRed/{}.DNG'.format(imgFileName)) as raw:
+def extractSpectrums(imgFileName, threshold):#, crop=None):
+    with rawpy.imread('images/imagesRed/{}.DNG'.format(imgFileName)) as raw:
         imgDim = raw.raw_image.shape
 
         img = raw.raw_image
@@ -86,48 +82,59 @@ def extractSpectrums(imgFileName, threshold, crop=None):
     #greenMask = np.logical_or((raw.raw_colors == 1), (raw.raw_colors == 3))
     greenMask = colors == 1
     blueMask = colors == 2
+        
+    numbersBB, spectrumBB = autoBB(img, threshold, redMask, greenMask, blueMask)
 
-    if crop is None:
-        numbersBB, spectrumBB = autocrop(img, threshold, redMask, greenMask, blueMask)
+    numbers = np.copy(img[numbersBB[1]:(numbersBB[1] + numbersBB[3]), numbersBB[0]:(numbersBB[0] + numbersBB[2])])
+    spectrum = np.copy(img[spectrumBB[1]:(spectrumBB[1] + spectrumBB[3]), spectrumBB[0]:(spectrumBB[0] + spectrumBB[2])])
 
-        numbers = np.copy(img[numbersBB[1]:(numbersBB[1] + numbersBB[3]), numbersBB[0]:(numbersBB[0] + numbersBB[2])])
-        spectrum = np.copy(img[spectrumBB[1]:(spectrumBB[1] + spectrumBB[3]), spectrumBB[0]:(spectrumBB[0] + spectrumBB[2])])
+    numbersRedMask = redMask[numbersBB[1]:(numbersBB[1] + numbersBB[3]), numbersBB[0]:(numbersBB[0] + numbersBB[2])]
 
-        numbersRedMask = redMask[numbersBB[1]:(numbersBB[1] + numbersBB[3]), numbersBB[0]:(numbersBB[0] + numbersBB[2])]
+    redMask = redMask[spectrumBB[1]:(spectrumBB[1] + spectrumBB[3]), spectrumBB[0]:(spectrumBB[0] + spectrumBB[2])]
+    greenMask = greenMask[spectrumBB[1]:(spectrumBB[1] + spectrumBB[3]), spectrumBB[0]:(spectrumBB[0] + spectrumBB[2])]
+    blueMask = blueMask[spectrumBB[1]:(spectrumBB[1] + spectrumBB[3]), spectrumBB[0]:(spectrumBB[0] + spectrumBB[2])]
 
-        redMask = redMask[spectrumBB[1]:(spectrumBB[1] + spectrumBB[3]), spectrumBB[0]:(spectrumBB[0] + spectrumBB[2])]
-        greenMask = greenMask[spectrumBB[1]:(spectrumBB[1] + spectrumBB[3]), spectrumBB[0]:(spectrumBB[0] + spectrumBB[2])]
-        blueMask = blueMask[spectrumBB[1]:(spectrumBB[1] + spectrumBB[3]), spectrumBB[0]:(spectrumBB[0] + spectrumBB[2])]
+    #if crop is None:
+    #    numbersBB, spectrumBB = autocrop(img, threshold, redMask, greenMask, blueMask)
 
-    else:
+    #    numbers = np.copy(img[numbersBB[1]:(numbersBB[1] + numbersBB[3]), numbersBB[0]:(numbersBB[0] + numbersBB[2])])
+    #    spectrum = np.copy(img[spectrumBB[1]:(spectrumBB[1] + spectrumBB[3]), spectrumBB[0]:(spectrumBB[0] + spectrumBB[2])])
 
-        print('crop :: {}'.format(crop))
-        print('dim :: {}'.format(imgDim))
-        height, width = imgDim
+    #    numbersRedMask = redMask[numbersBB[1]:(numbersBB[1] + numbersBB[3]), numbersBB[0]:(numbersBB[0] + numbersBB[2])]
 
-        numbersBB = np.round(np.array(crop[0:2]) * np.array([height, width])).astype('int32')
-        print('Numbers BB :: {}'.format(numbersBB))
+    #    redMask = redMask[spectrumBB[1]:(spectrumBB[1] + spectrumBB[3]), spectrumBB[0]:(spectrumBB[0] + spectrumBB[2])]
+    #    greenMask = greenMask[spectrumBB[1]:(spectrumBB[1] + spectrumBB[3]), spectrumBB[0]:(spectrumBB[0] + spectrumBB[2])]
+    #    blueMask = blueMask[spectrumBB[1]:(spectrumBB[1] + spectrumBB[3]), spectrumBB[0]:(spectrumBB[0] + spectrumBB[2])]
 
-        spectrumY = np.round(numbersBB[0, 0] + numbersBB[1, 0])
-        print('Spectrum Y :: {}'.format(spectrumY))
+    #else:
 
-        spectrumHeight = np.round(numbersBB[1, 0] * crop[2])
-        print('Spectrum Height :: {}'.format(spectrumHeight))
+    #    print('crop :: {}'.format(crop))
+    #    print('dim :: {}'.format(imgDim))
+    #    height, width = imgDim
 
-        spectrumBB = np.array([[spectrumY, numbersBB[0, 1]], [spectrumHeight, numbersBB[1][1]]]).astype('int32')
-        print('Spectrum BB :: {}'.format(spectrumBB))
+    #    numbersBB = np.round(np.array(crop[0:2]) * np.array([height, width])).astype('int32')
+    #    print('Numbers BB :: {}'.format(numbersBB))
 
-        numbersBB = numbersBB.flatten()
-        spectrumBB = spectrumBB.flatten()
+    #    spectrumY = np.round(numbersBB[0, 0] + numbersBB[1, 0])
+    #    print('Spectrum Y :: {}'.format(spectrumY))
 
-        numbers = np.copy(img[numbersBB[0]:(numbersBB[0] + numbersBB[2]), numbersBB[1]:(numbersBB[1] + numbersBB[3])])
-        spectrum = np.copy(img[spectrumBB[0]:(spectrumBB[0] + spectrumBB[2]), spectrumBB[1]:(spectrumBB[1] + spectrumBB[3])])
+    #    spectrumHeight = np.round(numbersBB[1, 0] * crop[2])
+    #    print('Spectrum Height :: {}'.format(spectrumHeight))
 
-        numbersRedMask = redMask[numbersBB[0]:(numbersBB[0] + numbersBB[2]), numbersBB[1]:(numbersBB[1] + numbersBB[3])]
+    #    spectrumBB = np.array([[spectrumY, numbersBB[0, 1]], [spectrumHeight, numbersBB[1][1]]]).astype('int32')
+    #    print('Spectrum BB :: {}'.format(spectrumBB))
 
-        redMask = redMask[spectrumBB[0]:(spectrumBB[0] + spectrumBB[2]), spectrumBB[1]:(spectrumBB[1] + spectrumBB[3])]
-        greenMask = greenMask[spectrumBB[0]:(spectrumBB[0] + spectrumBB[2]), spectrumBB[1]:(spectrumBB[1] + spectrumBB[3])]
-        blueMask = blueMask[spectrumBB[0]:(spectrumBB[0] + spectrumBB[2]), spectrumBB[1]:(spectrumBB[1] + spectrumBB[3])]
+    #    numbersBB = numbersBB.flatten()
+    #    spectrumBB = spectrumBB.flatten()
+
+    #    numbers = np.copy(img[numbersBB[0]:(numbersBB[0] + numbersBB[2]), numbersBB[1]:(numbersBB[1] + numbersBB[3])])
+    #    spectrum = np.copy(img[spectrumBB[0]:(spectrumBB[0] + spectrumBB[2]), spectrumBB[1]:(spectrumBB[1] + spectrumBB[3])])
+
+    #    numbersRedMask = redMask[numbersBB[0]:(numbersBB[0] + numbersBB[2]), numbersBB[1]:(numbersBB[1] + numbersBB[3])]
+
+    #    redMask = redMask[spectrumBB[0]:(spectrumBB[0] + spectrumBB[2]), spectrumBB[1]:(spectrumBB[1] + spectrumBB[3])]
+    #    greenMask = greenMask[spectrumBB[0]:(spectrumBB[0] + spectrumBB[2]), spectrumBB[1]:(spectrumBB[1] + spectrumBB[3])]
+    #    blueMask = blueMask[spectrumBB[0]:(spectrumBB[0] + spectrumBB[2]), spectrumBB[1]:(spectrumBB[1] + spectrumBB[3])]
 
 
 
@@ -135,13 +142,18 @@ def extractSpectrums(imgFileName, threshold, crop=None):
     print('Numbers Size :: {}'.format(numbersBB))
     print('Specturm Size :: {}'.format(spectrumBB))
 
-    numbers = amplify(stretch(numbers))
+
     #numbers = stretch(numbers)
-    spectrum = stretch(spectrum)
+    #spectrum = stretch(spectrum)
+    #cv2.imshow('numbers', numbers)
+    #cv2.imshow('spectrum', spectrum)
+    #cv2.waitKey(0)
+
+    #numbers = stretch(numbers)
     #divide = np.ones([1, numbers.shape[1]])
     #stacked = np.vstack([numbers, divide, spectrum])
 
-    #cv2.imshow('check :: {}'.format(random.random()), stretch(stacked))
+    #cv2.imshow('check', stretch(stacked))
     #cv2.waitKey(0)
 
 
@@ -175,3 +187,32 @@ def extractSpectrums(imgFileName, threshold, crop=None):
     blueMedians = blueMedians[:offByOneCheck]
 
     return [numbers, [redImg, redMedians], [greenImg, greenMedians], [blueImg, blueMedians]]
+
+def showSpectrum(imageSpectrumObject, name, wait=True):
+    numbers, red, green, blue = imageSpectrumObject
+    
+    stacked = np.vstack([numbers, red[0], green[0], blue[0]])
+    cv2.imshow('RGB {}'.format(name), stacked)
+    if wait:
+        cv2.waitKey(0)
+
+led = extractSpectrums(*ledImg)
+showSpectrum(led, 'led')
+
+#incA = extractSpectrums(incadecentAImg, 0.5)
+#showSpectrum(incA, 'Inc A', False)
+
+#incB = extractSpectrums(incadecentBImg, 0.5)
+#showSpectrum(incB, 'Inc B', False)
+
+#sky = extractSpectrums(skyImg, 0.5)
+#showSpectrum(sky, 'sky', False)
+
+#sun = extractSpectrums(sunDimImg, 0.08)
+#showSpectrum(sun, 'Sun', False)
+
+#benQ = extractSpectrums(benQImg, 0.1)
+#showSpectrum(benQ, 'ben Q', False)
+
+#iPad = extractSpectrums(iPadImg, 0.3)
+#showSpectrum(iPad, 'iPad')
